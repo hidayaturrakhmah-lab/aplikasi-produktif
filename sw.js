43681c9df44e7c5373cd015edbd833f5e66f130e
hidayaturrakhmah-lab <hidayaturrakhmah@gmail.com>
2025-12-21 14:27:45 +0700

    update

--- sw.js
+++ sw.js
@@ -2,21 +2,34 @@ const CACHE_NAME = 'aku-produktif-v1';
 const urlsToCache = [
   '/',
   '/index.html',
-  '/manifest.json'
+  '/manifest.json',
+  '/icons/icon-192.png',
+  '/icons/icon-512.png'
+  // Tambahkan file lain yang perlu di-cache (CSS, JS, dll)
 ];
 
+// Install Service Worker
 self.addEventListener('install', event => {
   event.waitUntil(
     caches.open(CACHE_NAME)
-      .then(cache => cache.addAll(urlsToCache))
+      .then(cache => {
+        console.log('Opened cache');
+        return cache.addAll(urlsToCache);
+      })
   );
 });
 
+// Fetch assets from cache
 self.addEventListener('fetch', event => {
   event.respondWith(
     caches.match(event.request)
       .then(response => {
-        return response || fetch(event.request);
-      })
+        // Cache hit - return response
+        if (response) {
+          return response;
+        }
+        return fetch(event.request);
+      }
+    )
   );
 });
